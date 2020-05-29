5f906ad235b5ec919cd2ded2876a636474a71d5a
Ercan Uzunsakal <ercan112358@gmail.com>
2020-05-29 12:34:02 +0300

    Font faces is added.

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,22 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Teko:700&display=swap&subset=latin-ext");
+@import url("https://fonts.googleapis.com/css?family=Barlow+Condensed|Righteous&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Racing+Sans+One&display=swap&subset=latin-ext");
+@import url("https://fonts.googleapis.com/css?family=Merriweather&display=swap");
+@font-face {
+  font-family: azonix;
+  src: local("azonix"), url(/assets/fonts/azonix.otf) format("otf");
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+.header {
+  z-index: 2;
+}
+</style>
