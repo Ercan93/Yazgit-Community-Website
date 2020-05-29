c8703ebd6b9125a29b7a6eb3ddddd61dbf6a1e3e
Ercan Uzunsakal <ercan112358@gmail.com>
2020-05-29 13:15:16 +0300

    FirstAIConf page is prepared.
    adn photos name renamed.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import About from "./views/About"
+import FirstAIConf from "./views/FirstAIConf"
 
 const routes = [{
     path: '/about',
     component: About,
     name: "aboutPage"
+},
+{
+    path: '/FirstAIConf',
+    component: FirstAIConf,
+    name: 'firstAiConf'
 }]
 
 export default routes;
---
+++ src/views/FirstAIConf.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <h1>1.Yapay Zeka Buluşmalarından Görüntüler</h1>
+    <img v-for="number in 13" v-bind:src="`${photos_path}${number}).jpg`" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      photos_path: "../src/assets/photos/Birinci_yapay_zeka_bulusmalari_konya_("
+    };
+  }
+};
+</script>
+<style scoped>
+.container {
+  display: inherit;
+  width: 95vw;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  background-image: url("../assets/background.svg");
+  background-size: 300%;
+}
+img {
+  margin-top: 30px;
+  width: 43vw;
+  margin-left: 45px;
+}
+h1 {
+  text-align: center;
+  margin-top: 20px;
+  font-family: "Merriweather";
+}
+</style>
