0a62dfad5bd80726c8c8b4c07ad69381ba5c6e9c
Ercan Uzunsakal <ercan112358@gmail.com>
2020-05-29 12:00:04 +0300

    About page is prepared.

--- src/store.js
+++ src/store.js
@@ -4,8 +4,14 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state: {},
-    getters: {},
+    state: {
+        aboutData:"Günümüzde her alanda kendini gösteren yapay zekânın geleceği şekillendiren teknoloji olacağı inancıyla çıktığımız yolda Yapay Zekâ ve Görüntü İşleme Topluluğu’muzu değerli danışmanımız Doç. Dr. Murat CEYLAN’ ın destekleri ve yönlendirmeleriyle 7 Mart 2018 tarihinde kurduk. Kurulduğumuz günden itibaren yapay zekâ alanında kendimizi geliştirmek adına Python yazılım dili kursu, yapay sinir ağları kursu, derin öğrenmeye giriş kursu, arduino kursu gibi çalışmaların yanı sıra yapay zekânın uygulama alanlarına ilişkin periyodik sunumlar, seminerler ve kitap okuma tartışma etkinlikleri gerçekleştirdik. Bununla birlikte farklı şehirlerde bulunan öğrenci toplulukları ve derneklerin düzenlediği konferanslara üyelerimizle birlikte katılım sağladık. Ulusal düzeyde tanınmakta olan topluluğumuz 'Yapay Zekâ Üreten Türkiye' anlayışıyla çeşitli proje grupları oluşturarak yapay zekanın farklı alanlarındaki uygulamalarını içeren laboratuvar çalışmalarını realize etmektedir.",
+    },
+    getters: {
+        aboutDataGetters(state){
+            return state.aboutData;
+        }
+    },
     mutations: {},
     actions: {}
 })
---
+++ src/views/About.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <h1 class="about-heading">Hakkımızda</h1>
+    <p class="about-text">{{about_data}}</p>
+    <div class="social-media">
+      <a href="https://twitter.com/yaz_git" target="_blank">
+        <img src="../assets/icons/Twitter.svg" width="50px" alt />
+        <p>Yaz_git</p>
+      </a>
+      <a href="https://www.instagram.com/yaz_git/" target="_blank">
+        <img src="../assets/icons/Instagram.svg" width="50px" alt />
+        <p>Yaz_git</p>
+      </a>
+      <a>
+        <img src="../assets/icons/Mail.svg" width="50px" alt />
+        <p>yazgit2018@gmail.com</p>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      about_data: null
+    };
+  },
+  computed() {
+    this.about_data = this.$store.getters("aboutDataGetters");
+  }
+};
+</script>
+<style scoped>
+.container {
+  height: 85vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Merriweather";
+  font-weight: bold;
+}
+.about-heading {
+  margin-top: 40px;
+}
+.social-media {
+  display: flex;
+  margin-bottom: 20px;
+}
+.social-media > a {
+  text-decoration: none;
+  color: black;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+}
+.social-media > a > p {
+  margin-left: 10px;
+}
+.about-text {
+  padding: 44px 100px 50px 100px;
+  font-size: 20px;
+  line-height: 40px;
+  text-align: center;
+}
+</style>
