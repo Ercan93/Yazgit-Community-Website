862ba9ed490038a94ed823429a666993638fa525
Ercan Uzunsakal <ercan112358@gmail.com>
2020-05-31 23:46:33 +0300

    SeconAIConf path is added in routes.

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,29 @@
 import About from "./views/About"
+import SecondAIConf from "./views/SecondAIConf"
 import FirstAIConf from "./views/FirstAIConf"
 import Activities from "./views/Activities"
 
-const routes = [{
-    path: '/about',
-    component: About,
-    name: "aboutPage"
-},
-{
-    path: '/FirstAIConf',
-    component: FirstAIConf,
-    name: 'firstAiConf'
-},
-{
-    path: '/Activities',
-    component: Activities,
-    name: 'activities'
-}
+const routes = [
+    {
+        path: '/',
+        component: SecondAIConf,
+        name: "secondAIConf"
+    },
+    {
+        path: '/about',
+        component: About,
+        name: "aboutPage"
+    },
+    {
+        path: '/FirstAIConf',
+        component: FirstAIConf,
+        name: 'firstAiConf'
+    },
+    {
+        path: '/Activities',
+        component: Activities,
+        name: 'activities'
+    }
 ]
 
 export default routes;
