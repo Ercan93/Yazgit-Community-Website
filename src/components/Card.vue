43e8c860c8db29c1e9b4f1dee7f9d0e9cb29c839
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-06 00:43:40 +0300

    Card.vue css classes prepared.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,7 @@
     <div v-for="speaker in speakersData" class="card">
       <div class="speaker">
         <a :href="`${speaker.address}`" target="blank"></a>
-        <img :src="`${speaker.imgSrc}`" />
+        <img class="speaker-img" :src="`${speaker.imgSrc}`" />
         <p class="speaker-name">{{speaker.name}}</p>
         <p class="speaker-profession">{{speaker.profession}}</p>
       </div>
@@ -20,3 +20,36 @@ export default {
   }
 };
 </script>
+<style scoped>
+.card {
+  border-radius: 20px;
+  width: 75vw;
+  height: 12rem;
+  margin-top: 60px;
+  margin-bottom: 40px;
+  padding: 20px;
+  display: flex;
+  align-items: stretch;
+}
+.speaker {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  margin-left: 10px;
+}
+.speaker-name {
+  font-size: 30px;
+  font-family: "Teko";
+}
+.speaker-profession {
+  font-size: 19px;
+  font-family: "Merriweather";
+}
+.speaker-img {
+  margin-right: 50px;
+  width: 160px;
+  height: auto;
+  border-radius: 15px;
+  border: 4px solid #004257;
+}
+</style>
