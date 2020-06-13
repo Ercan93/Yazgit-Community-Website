d853d6ba78b0e2524fd016c53e9a7bbe160d8a25
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-14 00:06:08 +0300

    Update NavRouterLink.vue

--- src/components/common/NavRouterLink.vue
+++ src/components/common/NavRouterLink.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>
-      <router-link class="nav-button" to="/FirstAIConf">
-        <a>1.Yapay Zeka Buluşmaları</a>
-      </router-link>
-      <router-link class="nav-button" to="/Activities">
-        <a>Etkinliklerimiz</a>
-      </router-link>
-      <router-link class="nav-button" to="About">
-        <a>Hakkımızda</a>
-      </router-link>
-    </div>
+  <div>
+    <router-link class="nav-button" to="/FirstAIConf">
+      <a>1.Yapay Zeka Buluşmaları</a>
+    </router-link>
+    <router-link class="nav-button" to="/Activities">
+      <a>Etkinliklerimiz</a>
+    </router-link>
+    <router-link class="nav-button" to="About">
+      <a>Hakkımızda</a>
+    </router-link>
+  </div>
 </template>
+
+<style scoped>
+.nav-button {
+  text-decoration: none;
+  color: white;
+  font-size: 20px;
+  margin-right: 40px;
+}
+.nav-button:hover {
+  background-color: rgba(0, 114, 180, 0.39);
+  padding: 10px 5px 10px 5px;
+  border-radius: 10px;
+}
+</style>
