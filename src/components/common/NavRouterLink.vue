3a88e0883d2d6dbc9ce962a1d2ee5f6a3ca4f110
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-13 23:59:27 +0300

    Create NavRouterLink.vue

---
+++ src/components/common/NavRouterLink.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <router-link class="nav-button" to="/FirstAIConf">
+        <a>1.Yapay Zeka Buluşmaları</a>
+      </router-link>
+      <router-link class="nav-button" to="/Activities">
+        <a>Etkinliklerimiz</a>
+      </router-link>
+      <router-link class="nav-button" to="About">
+        <a>Hakkımızda</a>
+      </router-link>
+    </div>
+</template>
