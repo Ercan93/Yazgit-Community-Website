2d7d3bc2354b2dff3853d5a630d0679416cce36d
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-18 23:15:40 +0300

    Update 404.vue

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="container">
+    <router-link class="back-to-home" to="/">
+      <p>Eve Dön</p>
+    </router-link>
     <img class="error" src="../assets/Error404.svg" alt />
   </div>
 </template>
 <style scoped>
 .container {
   display: flex;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-around;
 }
 .error {
-  width: 60vw;
+  height: 100vh;
+}
+.back-to-home {
+  font-family: "Righteous";
+  font-size: 10vh;
+  text-decoration: none;
+  color: #be234a;
 }
 </style>
