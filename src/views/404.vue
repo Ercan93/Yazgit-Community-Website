8533530f705b1a22d4895aa5f45871214e6a2f06
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-18 23:04:33 +0300

    Create 404.vue

---
+++ src/views/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <img class="error" src="../assets/Error404.svg" alt />
+  </div>
+</template>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+}
+.error {
+  width: 60vw;
+}
+</style>
